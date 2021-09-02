17508c858ed995ad52ff9d0ff3814f1ee17d44ba
nelsondcds <pipoca012008@yahoo.com>
2021-09-02 18:42:47 -0400

    initialized express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// app.use(express.static('public'));
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+	console.log(`API server now on port ${PORT}!`);
+});
