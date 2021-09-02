7949061d68c1d4fa62bb15d966ab1d52463d2419
nelsondcds <pipoca012008@yahoo.com>
2021-09-02 18:47:24 -0400

    connected to db on server.js and queried successfully

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // app.use(express.static('public'));
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
 app.listen(PORT, () => {
-	console.log(`API server now on port ${PORT}!`);
+	console.log(`Server running on port ${PORT}.`);
 });
